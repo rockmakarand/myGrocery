79821ba6ba8ac9da0ee39103bd7ac11f74bb8e3e
Shlok Upadhyay <deep.upu9@gmail.com>
2022-08-16 01:14:30 +0530

    Add: Navigator

--- myGrocery/App.js
+++ myGrocery/App.js
@@ -4,15 +4,20 @@ import Dashboard from './src/screens/Dashboard';
 import Search from './src/screens/SearchScreen';
 import Home from './src/screens/HomeScreen';
 import About from './src/screens/AboutScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Home />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="DashBoard" component={Dashboard} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default App;
