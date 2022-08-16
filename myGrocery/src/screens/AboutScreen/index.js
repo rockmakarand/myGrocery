37eadca2921bbd6ece00fd43645d5d05a3b6073e
Makarand <82673215+rockmakarand@users.noreply.github.com>
2022-08-16 13:01:56 +0530

    Update index.js

--- myGrocery/src/screens/AboutScreen/index.js
+++ myGrocery/src/screens/AboutScreen/index.js
@@ -11,6 +11,7 @@ import {
 } from 'react-native';
 
 const About = ({navigation}) => {
+ 
   return (
     <ScrollView style={styles.container}>
       <View
@@ -27,6 +28,7 @@ const About = ({navigation}) => {
             backgroundColor: '#24ce85',
             paddingTop: 50,
             fontWeight: 'bold',
+            color:'black'
           }}>
           About Us
         </Text>
@@ -38,47 +40,48 @@ const About = ({navigation}) => {
             fontWeight: 'bold',
             marginLeft: -270,
             marginTop: -30,
+            color:'black'
           }}>
           Back
         </Text>
       </View>
       <Text>{'\n'}</Text>
 
-      <Text style={{fontSize: 30, fontWeight: '500', padding: 20}}>
+      <Text style={{fontSize: 30, fontWeight: '500', padding: 20, color:'black'}}>
         myGrocery
       </Text>
 
-      <Image
-        source={require('../../assets/image_one.jpeg')}
-        style={{width: 347, height: 97, alignSelf: 'center'}}
-      />
-
-      <Text style={{fontSize: 30, fontWeight: '400', padding: 15}}>
-        myGrocery
-      </Text>
-
-      <Text style={{fontSize: 20, fontWeight: '700', padding: 15}}>
-        Trusted Company to Delight One's Heart Everyday
-      </Text>
-      <Text style={{fontSize: 20, fontWeight: '400', padding: 15}}>
-        Founded by Jeffery Cheah back in 2008. Since pandemic strikes he strives
-        to open an online store.
-      </Text>
-      <Text style={{fontSize: 20, fontWeight: '400', padding: 15}}>
-        Now, there are up to 10,000 orders made per day with myGrocery
-      </Text>
       <Image
         source={require('../../assets/image_two.jpeg')}
         style={{width: 347, height: 97, alignSelf: 'center'}}
       />
-      <Text style={{fontSize: 25, fontWeight: '700', padding: 15}}>
+
+      <Text style={{fontSize: 30, fontWeight: '400', padding: 15,color:'black'}}>
+        myGrocery
+      </Text>
+
+      <Text style={{fontSize: 20, fontWeight: '700', padding: 15,color:'black'}}>
+        Trusted Company to Delight One's Heart Everyday
+      </Text>
+      <Text style={{fontSize: 20, fontWeight: '400', padding: 15, color:'black'}}>
+        Founded by Jeffery Cheah back in 2008. Since pandemic strikes he strives
+        to open an online store.
+      </Text>
+      <Text style={{fontSize: 20, fontWeight: '400', padding: 15, color:'black'}}>
+        Now, there are up to 10,000 orders made per day with myGrocery
+      </Text>
+      <Image
+        source={require('../../assets/image_one.jpeg')}
+        style={{width: 347, height: 97, alignSelf: 'center'}}
+      />
+      <Text style={{fontSize: 25, fontWeight: '700', padding: 15, color:'black'}}>
         Founder
       </Text>
 
-      <Text style={{fontSize: 20, fontWeight: '400', padding: 15}}>
+      <Text style={{fontSize: 20, fontWeight: '400', padding: 15, color:'black'}}>
         Mr Jeffery Cheah
       </Text>
-      <Text style={{fontSize: 20, fontWeight: '400', padding: 15}}>
+      <Text style={{fontSize: 20, fontWeight: '400', padding: 15, color:'black'}}>
         From humble backgrounds, Mr Cheah at 25 aimed to transform his father’s
         grocery store into e-stores. Along with his co-founder, Ms Denise they
         had built one of the largest e-Grocery chain in Malaysia.
